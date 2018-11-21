704f350cc21335fca37de60ba2cdd198a679bbe0
stevecat <38344452+minblock@users.noreply.github.com>
2018-11-21 13:25:10 -0500

    Made executables execute

--- build-win.sh
+++ build-win.sh
@@ -1,7 +1,9 @@
 # build
 cd depends
+chown +x config.*
 make HOST=x86_64-w64-mingw32
 cd ..
+chmod +x *.sh
 ./autogen.sh # not required when building from tarball
 CONFIG_SITE=$PWD/depends/x86_64-w64-mingw32/share/config.site ./configure --prefix=/
 make
