9e6a9ed1f70361a0b45692f9595da1189668a3e9
stevecat <38344452+minblock@users.noreply.github.com>
2018-12-22 00:44:16 -0500

    Merged branch 14 into 0.14

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -42,10 +42,11 @@ const std::string CLIENT_NAME("PicscoinCore");
 #include "build.h"
 #endif
 
-//! git will put "#define GIT_ARCHIVE 1" on the next line inside archives. $Format:%n#define GIT_ARCHIVE 1$
+//! git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
+#define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
-#define GIT_COMMIT_ID "$Format:%h$"
-#define GIT_COMMIT_DATE "$Format:%cD$"
+#define GIT_COMMIT_ID "81e91e8"
+#define GIT_COMMIT_DATE "Fri, 21 Dec 2018 14:31:44 -0500"
 #endif
 
 #define BUILD_DESC_WITH_SUFFIX(maj, min, rev, build, suffix) \
