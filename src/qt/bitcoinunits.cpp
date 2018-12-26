4f24a8253529f1d9beb46d92f846904a118eb85f
Sever Neacsu <provg@Gigabeast.provgn.com>
2018-12-26 16:42:28 -0500

    Updated Currency Units
    Branding

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -61,8 +61,8 @@ QString BitcoinUnits::description(int unit)
     switch(unit)
     {
     case BTC: return QString("Picscoins");
-    case mBTC: return QString("Lites (1 / 1" THIN_SP_UTF8 "000)");
-    case uBTC: return QString("Photons (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
+    case mBTC: return QString("Pixels (1 / 1" THIN_SP_UTF8 "000)");
+    case uBTC: return QString("Bitmaps (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
     default: return QString("???");
     }
 }
