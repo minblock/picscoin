0da5a7ed00e61ff03348ef359462fff1aa2b66cc
minblock <sev@provgn.com>
2018-12-27 13:51:20 -0500

    Update to units

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -41,8 +41,8 @@ QString BitcoinUnits::name(int unit)
     switch(unit)
     {
     case BTC: return QString("PIC");
-    case mBTC: return QString("pixels");
-    case uBTC: return QString("bitmaps");
+    case mBTC: return QString("bitmaps");
+    case uBTC: return QString("pixels");
     default: return QString("???");
     }
 }
@@ -52,8 +52,8 @@ QString BitcoinUnits::description(int unit)
     switch(unit)
     {
     case BTC: return QString("Picscoins");
-    case mBTC: return QString("Pixels (1 / 1" THIN_SP_UTF8 "000)");
-    case uBTC: return QString("Bitmaps (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
+    case mBTC: return QString("Bitmaps (1 / 1" THIN_SP_UTF8 "000)");
+    case uBTC: return QString("Pixels (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
     default: return QString("???");
     }
 }
