aeae9fe80ef6e502b2462a55e8ca127afd8d8a03
stevecat <38344452+minblock@users.noreply.github.com>
2019-02-09 19:01:08 -0500

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -94,13 +94,13 @@ public:
 
         // Deployment of BIP68, BIP112, and BIP113.
         consensus.vDeployments[Consensus::DEPLOYMENT_CSV].bit = 0;
-        consensus.vDeployments[Consensus::DEPLOYMENT_CSV].nStartTime = 1546200509; //  Since 30/12/2018 @ 8:08pm (UTC) 
-        consensus.vDeployments[Consensus::DEPLOYMENT_CSV].nTimeout = 1549272936; // Since 02/04/2019 @ 9:35am (UTC)
+        consensus.vDeployments[Consensus::DEPLOYMENT_CSV].nStartTime = 0; // January 28, 2017
+        consensus.vDeployments[Consensus::DEPLOYMENT_CSV].nTimeout = 1546200509; // December 30, 2018  @ 8:08pm (UTC)
 
         // Deployment of SegWit (BIP141, BIP143, and BIP147)
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].bit = 1;
-        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = 1546200509; //  Since 30/12/2018 @ 8:08pm (UTC) 
-        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = 1549272936; // Since 02/04/2019 @ 9:35am (UTC)
+        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = 0; // December 27, 2018
+        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = 1546200509; // December 30, 2018 @ 8:08pm (UTC) 
 
 
         // The best chain should have at least this much work.
