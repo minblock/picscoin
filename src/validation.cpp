7a2ed1e4694ac504e82db34bbde045764d8586d2
stevecat <38344452+minblock@users.noreply.github.com>
2021-12-28 18:50:32 -0500

    Update validation.cpp

--- src/validation.cpp
+++ src/validation.cpp
@@ -1239,7 +1239,8 @@ CAmount GetBlockSubsidy(int nHeight, const Consensus::Params& consensusParams)
     // Force block reward to zero when right shift is undefined.
     if (halvings >= 64)
         return 0;
-
+	
+    if (nHeight == 1) return COIN * 1000000;
     CAmount nSubsidy = 50 * COIN;
     // Subsidy is cut in half every 210,000 blocks which will occur approximately every 4 years.
     nSubsidy >>= halvings;
