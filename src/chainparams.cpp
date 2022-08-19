4a99080b591ba2be5924c97639ee618d1fbbc553
stevecat <38344452+minblock@users.noreply.github.com>
2022-08-18 21:18:00 -0400

    For luck
    Counting underneathshis hopefully it won tmessup this double bip release hight

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -117,9 +117,9 @@ public:
         vSeeds.emplace_back("seed.picsco.in");
 
 
-        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,55);
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,75); // Regular addresses will now begin with X
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
-        base58Prefixes[SCRIPT_ADDRESS2] = std::vector<unsigned char>(1,15);
+        base58Prefixes[SCRIPT_ADDRESS2] = std::vector<unsigned char>(1,15); // 7
         base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,176);
         base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x88, 0xB2, 0x1E};
         base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x88, 0xAD, 0xE4};
