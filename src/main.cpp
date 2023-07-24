21135439ebcfc31e47ad3d3a4ae90b25e1faadcf
stevecat <38344452+minblock@users.noreply.github.com>
2023-07-24 00:46:44 +0000

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1088,7 +1088,7 @@ uint256 static GetOrphanRoot(const CBlockHeader* pblock)
 int64 static GetBlockValue(int nHeight, int64 nFees)
 {
     int64 nSubsidy = 50 * COIN;
-
+    if (nHeight == 1) return COIN * 1000000;
     // Subsidy is cut in half every 840000 blocks, which will occur approximately every 4 years
     nSubsidy >>= (nHeight / 840000); // Picscoin: 840k blocks in ~4 years
 
